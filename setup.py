1200dc11787f6c85a23bb4ab6bf55b88a1506651
Martin Martimeo <martin@martimeo.de>
2014-09-15 12:42:22 +0200

    Adding MANIFEST file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 
 setup(
     name='Tornado-Menumaker',
-    version='0.1.1',
+    version='0.1.2',
     author='Martin Martimeo',
     author_email='martin@martimeo.de',
     url='https://github.com/MartinMartimeo/tornado-menumaker',
